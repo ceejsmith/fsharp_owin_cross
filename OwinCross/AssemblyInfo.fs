4856788cef3e564c64206f390b725c05c380b867
Chris Smith <csmith@scottlogic.com>
2016-02-09 14:43:30 +0000

    Initial commit
    This was created in MonoDevelop on Ubuntu

---
+++ OwinCross/AssemblyInfo.fs
@@ -0,0 +1,18 @@
+﻿module OwinCross.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly:AssemblyTitle("OwinCross")>]
+[<assembly:AssemblyDescription("")>]
+[<assembly:AssemblyConfiguration("")>]
+[<assembly:AssemblyCompany("")>]
+[<assembly:AssemblyProduct("")>]
+[<assembly:AssemblyCopyright("chris")>]
+[<assembly:AssemblyTrademark("")>]
+[<assembly:AssemblyVersion("1.0.0.0")>]
+()
+// The assembly version has the format {Major}.{Minor}.{Build}.{Revision}
+//[<assembly: AssemblyDelaySign(false)>]
+//[<assembly: AssemblyKeyFile("")>]
+
---
+++ OwinCross/Program.fs
@@ -0,0 +1,32 @@
+﻿open Owin
+open Microsoft.Owin.Hosting
+
+open System
+open System.Web.Http
+open System.Collections.Generic
+
+type Startup() =
+   member x.Configuration (appBuilder: IAppBuilder) =
+       let config = new HttpConfiguration()
+       let routes = config.Routes
+       let route = routes.MapHttpRoute("DefaultApi", "api/{controller}/{id}")
+       route.Defaults.Add("id", RouteParameter.Optional)
+       appBuilder.UseWebApi(config) |> ignore
+
+type ValuesController() =
+    inherit ApiController()
+    member x.Get() =
+        printfn "Received Get for Values"
+        seq { yield "value1"; yield "value2" }
+    member x.Get(id: int) =
+        printfn "Received Get for Value %d" id
+        sprintf "value %d" id
+
+[<EntryPoint>]
+let main _ =
+    let baseAddress = "http://localhost:9000"
+    use webapp = WebApp.Start<Startup>(baseAddress)
+    printfn "Running on %s" baseAddress
+    printfn "Press enter to exit"
+    Console.ReadLine() |> ignore
+    0    
